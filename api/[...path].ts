598535d1c29fd64d5f0dbe8ac8aae7ef2c1254db
Mahir Kachwala <kachwalamahir2005@gmail.com>
2026-03-28 01:29:28 +0530

    Update [...path].ts

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,4 +1,4 @@
-import handler from "../server/mockApiHandler.js";
+import { handleApiRequest } from "../server/mockApiHandler.js";
 export default async function handler(req: any, res: any) {
   await handleApiRequest(req, res);
 }
